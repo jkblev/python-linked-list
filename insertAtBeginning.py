2323d9f719b7bce1ff26c22cd562c7ea5587e209
Jess Blevins <jessica.k.blevins@gmail.com>
2016-05-16 19:05:37 -0400

    create insertAtBeginning.py
    https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list

---
+++ insertAtBeginning.py
@@ -0,0 +1,21 @@
+"""
+ Insert Node at the begining of a linked list
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method. 
+"""
+
+def Insert(head, data):
+    newNode = Node(data, next_node=None)
+    if head == None:
+        return newNode
+    newNode.next = head
+    return newNode
+        
